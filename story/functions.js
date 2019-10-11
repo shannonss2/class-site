b50146dd5a9fc5e7dbecd1cbe67577f0380ba577
Skye Shannon <shannonss2@vcu.edu>
2019-10-11 02:27:06 -0400

    beginnning of story

---
+++ story/functions.js
@@ -0,0 +1,24 @@
+function one() {
+var element = document.getElementById("cloud-1");
+element.classList.toggle("cloud-1-move");
+}
+
+function two() {
+var element = document.getElementById("cloud-2");
+element.classList.toggle("cloud-2-move");
+}
+
+function three() {
+var element = document.getElementById("ocean");
+element.classList.toggle("ocean-move");
+}
+
+function four() {
+var element = document.getElementById("sky");
+element.classList.toggle("sky-move");
+}
+
+function five() {
+var element = document.getElementById("sun");
+element.classList.toggle("sun-move");
+}
