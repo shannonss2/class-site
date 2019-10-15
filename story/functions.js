6c094bb5e03f820e8ab7594bc18bd3011bf99dc4
Skye Shannon <shannonss2@vcu.edu>
2019-10-14 21:29:44 -0400

    story finalized

--- story/functions.js
+++ story/functions.js
@@ -1,5 +1,5 @@
 function one() {
-var element = document.getElementById("cloud-1");
+var element = document.querySelector(".cloud-1");
 element.classList.toggle("cloud-1-move");
 }
 
@@ -21,4 +21,102 @@ element.classList.toggle("sky-move");
 function five() {
 var element = document.getElementById("sun");
 element.classList.toggle("sun-move");
+}
+
+function six() {
+var element = document.querySelector(".bird-1");
+element.classList.toggle("bird-1-move");
+}
+
+function seven() {
+var element = document.querySelector(".bird-2");
+element.classList.toggle("bird-2-move");
+}
+
+
+function eight() {
+var element = document.getElementById("feather-1");
+element.classList.toggle("feather-1-move");
+}
+
+function nine() {
+var element = document.getElementById("feather-2");
+element.classList.toggle("feather-2-move");
+}
+
+function ten() {
+var element = document.getElementById("feather-3");
+element.classList.toggle("feather-3-move");
+}
+
+function eleven() {
+var element = document.querySelector(".icarus-flying");
+element.classList.toggle("icarus-flying-move");
+var element2 = document.getElementById("bird-1");
+element2.classList.toggle("bird-1-gone");
+}
+
+function twelve() {
+var element = document.getElementById("icarus-falling");
+element.classList.toggle("icarus-falling-move");
+var element2 = document.querySelector(".icarus-flying");
+element2.classList.toggle("icarus-flying-gone");
+}
+
+function thirteen() {
+var element = document.getElementById("splash");
+element.classList.toggle("splash-move");
+var element2 = document.getElementById("icarus-falling");
+element2.classList.toggle("icarus-falling-gone");
+}
+
+function fourteen() {
+var element2 = document.getElementById("splash");
+element2.classList.toggle("splash-gone");
+}
+
+function fifteen() {
+var element = document.querySelector(".bird-1-2");
+element.classList.toggle("bird-1-2-move");
+var element2 = document.querySelector(".bird-1");
+element2.classList.toggle("bird-1-gone");
+}
+
+function sixteen() {
+var element = document.querySelector(".bird-2-2");
+element.classList.toggle("bird-2-2-move");
+var element2 = document.querySelector(".bird-2");
+element2.classList.toggle("bird-2-gone");
+}
+
+function seventeen() {
+var element = document.querySelector(".sun2");
+element.classList.toggle("sun2-move");
+var element2 = document.getElementById("sun");
+element2.classList.toggle("sun-gone");
+}
+
+function eighteen() {
+var element = document.querySelector(".cloud-2-2");
+element.classList.toggle("cloud-2-2-move");
+var element2 = document.getElementById("cloud-2");
+element2.classList.toggle("cloud-2-gone");
+}
+
+function nineteen() {
+var element = document.querySelector(".cloud-1-2");
+element.classList.toggle("cloud-1-2-move");
+var element2 = document.querySelector(".cloud-1");
+element2.classList.toggle("cloud-1-gone");
+}
+
+function twenty() {
+var element = document.querySelector(".sky-1");
+element.classList.toggle("sky-1-move");
+var element2 = document.getElementById("sky");
+element2.classList.toggle("sky-gone");
+var element3 = document.querySelector(".ocean-1");
+element3.classList.toggle("ocean-1-move");
+var element4 = document.getElementById("ocean");
+element4.classList.toggle("ocean-gone");
 }
